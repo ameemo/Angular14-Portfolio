f5cd107e8c01a79b644138273c70e23cfe51d757
America Monzalvo <amonzalvo@07-MacBook-Air-de-America.local>
2023-03-02 10:01:23 -0600

    (FIX) Change the main name of project in angular

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app-piano'),
+      dir: require('path').join(__dirname, './coverage/angular14-portfolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
